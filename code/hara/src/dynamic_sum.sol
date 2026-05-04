6bf39256e387a4531cd229e009dc59d472b2b0a0
haradejene <haradejeneleta@gmail.com>
2026-05-04 23:04:42 +0300

    dynamic sum (arrays) exercise and some module fixes

--- code/hara/src/dynamic_sum.sol
+++ code/hara/src/dynamic_sum.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.8.20;
 
 contract Contract {
-    function sum(uint[5] memory numbers) external pure returns(uint total) {
+    function sum(uint[] calldata numbers) external pure returns(uint total) {
         for(uint i = 0; i < numbers.length; i++) {
             total += numbers[i];
         }
