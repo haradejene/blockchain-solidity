0dcff520073adac51e8762df2928df1a4b2b25dc
haradejene <haradejeneleta@gmail.com>
2026-05-04 23:56:55 +0300

    stack club (adding members) exercise

---
+++ code/hara/src/stack_club.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.20;
+
+contract StackClub {
+    address[] public members;
+    
+    function addMember(address newMember) external {
+        members.push(newMember);
+    }
+    
+    function isMember(address member) public view returns(bool) {
+        for(uint i = 0; i < members.length; i++) {
+            if(members[i] == member) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
