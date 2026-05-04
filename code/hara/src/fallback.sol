2fd3f974b0a662b44563fa703c10e45310cc820a
haradejene <haradejeneleta@gmail.com>
2026-05-04 22:18:53 +0300

    fall back exercise

---
+++ code/hara/src/fallback.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.20;
+
+contract Sidekick {
+    function makeContact(address hero) external {
+        (bool success, ) = hero.call(abi.encodeWithSignature(""));
+        require(success);
+    }
+}
