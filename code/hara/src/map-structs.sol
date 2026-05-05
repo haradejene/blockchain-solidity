7c14068698a6512a130af5aedab939e189e09d23
haradejene <haradejeneleta@gmail.com>
2026-05-05 18:45:38 +0300

    mapping to structs exercise

---
+++ code/hara/src/map-structs.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.20;
+
+contract Contract {
+	struct User {
+		uint balance;
+		bool isActive;
+	}
+	
+	mapping(address => User) public users;
+	
+	function createUser() external {
+		require(!users[msg.sender].isActive, "User already exists and is active");
+		
+		users[msg.sender] = User({
+			balance: 100,
+			isActive: true
+		});
+	}
+}
