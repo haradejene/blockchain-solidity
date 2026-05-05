d8187e366db0ec07aae27d5bc28da95da6bdcac8
haradejene <haradejeneleta@gmail.com>
2026-05-05 18:42:33 +0300

    added mapping exercise

--- code/hara/src/mapping.sol
+++ code/hara/src/mapping.sol
@@ -7,6 +7,10 @@ contract Contract {
         members[_member] = true;
     }
     
+    function removeMember(address _member) external {
+        members[_member] = false;
+    }
+    
     function isMember(address _member) external view returns(bool) {
         return members[_member];
     }
