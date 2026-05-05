8b4359d7076772cdb80de58e98e15fc18e8ba1da
haradejene <haradejeneleta@gmail.com>
2026-05-05 21:49:28 +0300

    proposal for voting contracts

---
+++ code/hara/src/voting_contract.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.8.20;
+
+contract Voting {
+    struct Proposal {
+        address target;
+        bytes data;
+        uint yesCount;
+        uint noCount;
+    }
+    
+    Proposal[] public proposals;
+    
+    function newProposal(address target, bytes calldata data) external {
+        Proposal memory newProposal = Proposal({
+            target: target,
+            data: data,
+            yesCount: 0,
+            noCount: 0
+        });
+        
+        proposals.push(newProposal);
+    }
+}
